13c667baab733e20b654667b33eee96bca3f0354
Nadiya <nadiyanzmp@gmail.com>
2022-07-10 20:33:41 +0400

    example_003_Summa

---
+++ example_003_Summa/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numA = 3;
+int numB = 33;
+int result = numA + numB;
+Console.WriteLine(result); 
+// Данное решение можно осуществить сложением двух переменных, 
+//либо создать промежуточную переменнуюБ ей присвоить сложение двух переменных и после 
+//вывести вывести промежуточную переменную
